d0817cbb26cebd0611bc2e23711574e8a6504572
dancingcodes <dancingcodes@qq.com>
2025-08-14 17:58:10 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,15 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-router.get('/img', async (req, res) => {
-    const r = await axios.get('https://api.52vmy.cn/api/img/tu/girl')
-    res.json({ data: r.data.url });
-});
-
-router.get('/img1', async (req, res) => {
-    const r = await axios.get('https://api.lolicon.app/setu/v2?tag=fgo&num=1&r18=0')
-    res.json({ data: r.data.data });
-});
-
 router.get('/video', async (req, res) => {
-    const r = await axios.get('https://api.kuleu.com/api/MP4_xiaojiejie?type=json')
-    res.json({ data: r.data.mp4_video });
+    const requests = Array.from({ length: 10 }, () =>
+        axios.get('https://api.kuleu.com/api/MP4_xiaojiejie?type=json')
+    );
+    const results = await Promise.all(requests);
+    // 提取 mp4_video
+    const videos = results.map(r => r.data.mp4_video);
+    // 返回结果
+    res.json({ data: videos });
 });
-
 
 module.exports = router;
